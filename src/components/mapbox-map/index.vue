1dc84310bb68a61306b66490216edaa187da994e
langelova <urb.lili.an@gmail.com>
2019-11-18 14:52:43 +0100

    new map

---
+++ src/components/mapbox-map/index.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="mapbox-map">
+  <mgl-map
+    mapStyle="mapbox://styles/mapbox/streets-v11"
+    :accessToken="mapboxToken"
+  >
+
+  </mgl-map>
+</div>
+</template>
+
+<script>
+import 'mapbox-gl/dist/mapbox-gl.css';
+import Mapbox from 'mapbox-gl';
+import { MglMap } from 'vue-mapbox';
+
+export default {
+  components: {
+    MglMap
+  },
+
+  computed: {
+    mapboxToken() {
+      return process.env.VUE_APP_MAPBOX_TOKEN;
+    }
+  },
+
+  created() {
+    this.mapbox = Mapbox;
+  }
+};
+</script>
+
+<style>
+.mapbox-map {
+  width: 100%;
+  height: 100%;
+}
+</style>
