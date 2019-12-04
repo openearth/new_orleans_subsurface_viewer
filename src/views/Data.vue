df68aa7c20f45d56b46d8f5e9d42190055cd0c2e
Bob Bol <bob@voorhoede.nl>
2019-12-04 15:26:55 +0100

    drawing a line on a map!!

--- src/views/Data.vue
+++ src/views/Data.vue
@@ -1,11 +1,172 @@
 <template>
-  <div>
-    Data
+  <div class="pa-2">
+    <v-btn
+      @click="toggleDrawLine"
+      :disabled="isDrawing"
+    >
+      {{ hasFullyDrawnLine ? 'Re-d' : 'D' }}raw line
+    </v-btn>
+    <pre>{{ linestring }}</pre>
   </div>
 </template>
 
 <script>
-export default {
+const DELTARES_BLUE = '#008fc5';
+const SOURCE_NAME = 'draw-geojson';
+const POINTS_LAYER_ID = 'draw-points';
+const LINES_LAYER_ID = 'draw-lines';
+const MAX_POINTS = 2;
 
+export default {
+  data: () => ({
+    geojson: {
+      'type': 'FeatureCollection',
+      'features': []
+    },
+    linestring: {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'LineString',
+        'coordinates': []
+      }
+    },
+    isDrawing: false
+  }),
+
+  computed: {
+    hasFullyDrawnLine() {
+      return this.geojson.features.length > MAX_POINTS;
+    }
+  },
+
+  methods: {
+    clickFn(e) {
+      const map = this.$root.map;
+      const features = map.queryRenderedFeatures(e.point, { layers: [POINTS_LAYER_ID] });
+
+      // Remove the linestring from the group
+      // So we can redraw it based on the points collection
+      // (linestring is always the last element)
+      if(this.geojson.features.length > 1) this.geojson.features.pop();
+
+      // If a feature was clicked, remove it from the map
+      if(features.length) {
+        const id = features[0].properties.id;
+        this.geojson.features = this.geojson.features.filter(point =>
+          point.properties.id !== id
+        );
+      // Otherwise we add a point
+      }
+      else {
+        const point = {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [
+              e.lngLat.lng,
+              e.lngLat.lat
+            ]
+          },
+          "properties": {
+            "id": String(new Date().getTime())
+          }
+        };
+        this.geojson.features.push(point);
+      }
+
+      // Build the line
+      if(this.geojson.features.length > 1) {
+        this.linestring.geometry.coordinates = this.geojson.features.map(point =>
+          point.geometry.coordinates
+        );
+
+        this.geojson.features.push(this.linestring);
+      }
+
+      map.getSource(SOURCE_NAME).setData(this.geojson);
+
+      // If we reach the maximum amount of points, stop drawing
+      if(this.hasFullyDrawnLine) {
+        this.cleanListeners();
+        this.isDrawing = false;
+      }
+    },
+
+    mouseMoveFn(e) {
+      const map = this.$root.map;
+      const features = map.queryRenderedFeatures(e.point, { layers: [POINTS_LAYER_ID] });
+      // UI indicator for clicking/hovering a point on the map
+      map.getCanvas().style.cursor = features.length ? 'pointer' : 'crosshair';
+    },
+
+    toggleDrawLine() {
+      if(!this.isDrawing) {
+        this.initDrawLine();
+        this.isDrawing = true;
+      }
+    },
+
+    cleanListeners() {
+      console.log('cleanListeners');
+      const map = this.$root.map;
+      map.off('click', this.clickFn);
+      map.off('mousemove', this.mouseMoveFn);
+      map.getCanvas().style.cursor = '';
+    },
+
+    removeDrawing() {
+      this.geojson.features = [];
+      this.linestring.geometry.coordinates = [];
+
+      const map = this.$root.map;
+      if(map.getLayer(POINTS_LAYER_ID)) map.removeLayer(POINTS_LAYER_ID);
+      if(map.getLayer(LINES_LAYER_ID)) map.removeLayer(LINES_LAYER_ID);
+      if(map.getSource(SOURCE_NAME)) map.removeSource(SOURCE_NAME);
+    },
+
+    initDrawLine() {
+      const map = this.$root.map;
+      this.removeDrawing();
+
+      map.addSource(SOURCE_NAME, {
+        'type': 'geojson',
+        'data': this.geojson
+      });
+
+      map.addLayer({
+        id: POINTS_LAYER_ID,
+        type: 'circle',
+        source: SOURCE_NAME,
+        paint: {
+          'circle-radius': 5,
+          'circle-color': DELTARES_BLUE
+        },
+        filter: ['in', '$type', 'Point']
+      });
+
+      map.addLayer({
+        id: LINES_LAYER_ID,
+        type: 'line',
+        source: SOURCE_NAME,
+        layout: {
+          'line-cap': 'round',
+          'line-join': 'round'
+        },
+        paint: {
+          'line-color': DELTARES_BLUE,
+          'line-width': 2.5
+        },
+        filter: ['in', '$type', 'LineString']
+      });
+
+      map.on('click', this.clickFn);
+      map.on('mousemove', this.mouseMoveFn);
+    }
+  },
+
+  destroyed() {
+    this.cleanListeners();
+    this.removeDrawing();
+  }
 };
 </script>
