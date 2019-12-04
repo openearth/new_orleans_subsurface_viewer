f3acea32f247be92760d557c1005fa0b70f3a3d0
Bob Bol <bob@voorhoede.nl>
2019-12-04 11:41:21 +0100

    Routes :: reset map on route :: tabbed router links

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,54 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import store from '@/store';
 
-// import Home from '@/views/Home';
 import Geology from '@/views/Geology';
+import Model from '@/views/Model';
+import Data from '@/views/Data';
+import Monitoring from '@/views/Monitoring';
+import Subsidence from '@/views/Subsidence';
 
 Vue.use(VueRouter);
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'home',
-  //   component: Home
-  // },
   {
     path: '/',
-    // path: '/geology',
+    redirect: '/geology'
+  },
+  {
+    path: '/geology',
     name: 'geology',
     component: Geology
+  },
+  {
+    path: '/model',
+    name: 'model',
+    component: Model
+  },
+  {
+    path: '/data',
+    name: 'data',
+    component: Data
+  },
+  {
+    path: '/monitoring',
+    name: 'monitoring',
+    component: Monitoring
+  },
+  {
+    path: '/subsidence',
+    name: 'subsidence',
+    component: Subsidence
   }
 ];
 
 const router = new VueRouter({
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  store.commit('mapbox/RESET_ALL');
+  next();
 });
 
 export default router;
---
+++ src/views/Subsidence.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Subsidence
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
