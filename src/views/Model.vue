104a88caa2d751b54bca692b6e0fc56947039384
hendriksen <gerrit.hendriksen@deltares.nl>
2025-09-22 17:26:43 +0200

    change of ground water model. reference to report added as well as mail address for further information

--- src/views/Model.vue
+++ src/views/Model.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
     <v-card-title>
-      Groundwater Model
+      New Orleans Community Ground Water Model
     </v-card-title>
     <v-subheader>
-      Coming soon
+      A report describing the model may be found at this <a href="https://new-orleans.openearth.eu/static/documents/DeltaresUSA GW Modelling Report.pdf" target="_blank">link</a>.
+
+      The model parameters and grid are freely available.
+      If you would like the model grid and parameters, please contact Deltares USA at the following email address.
+      <a href="mailto:info@deltares-usa.us">info@deltares-usa.us</a>
     </v-subheader>
     <v-sheet class="pa-5">
 
