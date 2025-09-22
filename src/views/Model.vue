81d33a465beacd71013c65610dc3e67f917d0103
hendriksen <gerrit.hendriksen@deltares.nl>
2025-09-22 19:55:34 +0200

    small breaks

--- src/views/Model.vue
+++ src/views/Model.vue
@@ -4,16 +4,17 @@
       New Orleans Community Ground Water Model
     </v-card-title>
     <v-card-text>
+      <br>
       A report describing the model may be found at this <a href="https://new-orleans.openearth.eu/static/documents/DeltaresUSA GW Modelling Report.pdf" target="_blank">link</a>.
-
+      <br>
       The model parameters and grid are freely available.
       If you would like the model grid and parameters, please contact Deltares USA at the following email address.
+      <br>
       <a href="mailto:info@deltares-usa.us">info@deltares-usa.us</a>
     </v-card-text>
     <v-subheader>
     </v-subheader>
     <v-sheet class="pa-5">
-
     </v-sheet>
   </div>
 </template>
