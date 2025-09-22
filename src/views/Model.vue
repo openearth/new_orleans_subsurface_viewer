0a5322a38fc201af57a84c81217a3c0dced25cb6
hendriksen <gerrit.hendriksen@deltares.nl>
2025-09-22 17:32:33 +0200

    improved reading by using v-card-text

--- src/views/Model.vue
+++ src/views/Model.vue
@@ -3,12 +3,14 @@
     <v-card-title>
       New Orleans Community Ground Water Model
     </v-card-title>
-    <v-subheader>
+    <v-card-text>
       A report describing the model may be found at this <a href="https://new-orleans.openearth.eu/static/documents/DeltaresUSA GW Modelling Report.pdf" target="_blank">link</a>.
 
       The model parameters and grid are freely available.
       If you would like the model grid and parameters, please contact Deltares USA at the following email address.
       <a href="mailto:info@deltares-usa.us">info@deltares-usa.us</a>
+    </v-card-text>
+    <v-subheader>
     </v-subheader>
     <v-sheet class="pa-5">
 
